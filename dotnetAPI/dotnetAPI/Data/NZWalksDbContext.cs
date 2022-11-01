5bc991aab44de0dcf2b066eaa04c2c25d0ac17a1
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2022-11-01 14:29:14 -0700

    Db context added

---
+++ dotnetAPI/dotnetAPI/Data/NZWalksDbContext.cs
@@ -0,0 +1,17 @@
+﻿using dotnetAPI.model;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnetAPI.dao
+{
+    public class NZWalksDbContext: DbContext
+    {
+        public NZWalksDbContext(DbContextOptions<NZWalksDbContext> options): base(options)
+        {
+
+        }
+        //DbSet is to create a table in the DB if it does not exist
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+    }
+}
