35e59b7b22a08509f3baf695f6b527b134e701db
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2022-11-01 12:18:58 -0700

    Walk model added

---
+++ dotnetAPI/dotnetAPI/model/Walk.cs
@@ -0,0 +1,17 @@
+﻿namespace dotnetAPI.model
+{
+    public class Walk
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Length { get; set; }
+        public int RegionId { get; set; }
+        public int WalkDifficultyId { get; set; }
+
+        //navigation prop
+
+        public Region Region { get; set; }
+        public WalkDifficulty WalkDifficulty { get; set; }
+
+    }
+}
