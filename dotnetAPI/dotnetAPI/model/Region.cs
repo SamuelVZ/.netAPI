3eb122c049b49e519f23128a9261d9dcac6aba61
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2022-11-01 12:18:22 -0700

    Region model added

---
+++ dotnetAPI/dotnetAPI/model/Region.cs
@@ -0,0 +1,15 @@
+﻿namespace dotnetAPI.model
+{
+    public class Region
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public double Lat { get; set; }
+        public double Long { get; set; }
+        public long Population { get; set; }
+
+        //navigation property
+        public IEnumerable<Walk> Walks { get; set; }
+    }
+}
