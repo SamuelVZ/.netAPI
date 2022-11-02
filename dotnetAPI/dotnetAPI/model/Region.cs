98beb78f12cec2f3c6f058e8d4462c0ad5dd4bd7
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2022-11-02 14:07:48 -0700

    updated region model

--- dotnetAPI/dotnetAPI/model/Region.cs
+++ dotnetAPI/dotnetAPI/model/Region.cs
@@ -5,6 +5,7 @@
         public int Id { get; set; }
         public string Code { get; set; }
         public string Name { get; set; }
+        public double Area { get; set; }
         public double Lat { get; set; }
         public double Long { get; set; }
         public long Population { get; set; }
