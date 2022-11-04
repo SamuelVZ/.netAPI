bbebb3a5f256082a63786344457890dc2e02bb85
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2022-11-04 13:47:47 -0700

    mapper used on the controller

--- dotnetAPI/dotnetAPI/Controllers/RegionsController.cs
+++ dotnetAPI/dotnetAPI/Controllers/RegionsController.cs
@@ -1,4 +1,6 @@
-﻿using dotnetAPI.DTO;
+﻿using AutoMapper;
+using dotnetAPI.DTO;
+using dotnetAPI.model;
 using dotnetAPI.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,30 +9,37 @@ namespace dotnetAPI.Controllers {
     [Route("[controller]")] //route needed to call this controller ex http://localhost:4200/regions
     public class RegionsController : Controller {
         private readonly RegionRepository regionRepository;
+        private readonly IMapper mapper;
 
-        public RegionsController(RegionRepository regionRepository) {
+        public RegionsController(RegionRepository regionRepository, IMapper mapper) {
             this.regionRepository = regionRepository;
+            this.mapper = mapper;
         }
         [HttpGet]
         public IActionResult GetRegions() {
             var regions = regionRepository.GetAll();
-            var regionsDto = new List<RegionDto>();
 
-            regions.ToList().ForEach(region => {
-                var regionDto = new RegionDto() {
-                    Id = region.Id,
-                    Code = region.Code,
-                    Name = region.Name,
-                    Area = region.Area,
-                    Lat = region.Lat,
-                    Long = region.Long,
-                    Population = region.Population
+            //var regionsDto = new List<RegionDto>();
 
-                };
+            //regions.ToList().ForEach(region => {
+            //    var regionDto = new RegionDto() {
+            //        Id = region.Id,
+            //        Code = region.Code,
+            //        Name = region.Name,
+            //        Area = region.Area,
+            //        Lat = region.Lat,
+            //        Long = region.Long,
+            //        Population = region.Population
 
-                regionsDto.Add(regionDto);
+            //    };
 
-            });
+            //    regionsDto.Add(regionDto);
+
+            //});
+
+
+            var regionsDto = mapper.Map<List<Region>>(regions);
+
             return Ok(regionsDto);
 
 
