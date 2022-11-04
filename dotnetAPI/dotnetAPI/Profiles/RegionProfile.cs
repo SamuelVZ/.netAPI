98f8758988746dcbe9100d85b1253b1f6c6a815f
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2022-11-04 12:57:27 -0700

    RegionProfile added for mapping

---
+++ dotnetAPI/dotnetAPI/Profiles/RegionProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using dotnetAPI.DTO;
+using dotnetAPI.model;
+
+namespace dotnetAPI.Profiles {
+    //Mapper to convert to DTO
+    public class RegionProfile: Profile {
+        public RegionProfile() {
+            //maps same name attributes
+            CreateMap<Region, RegionDto>();
+
+
+
+            //this option is in case you map attributes have different names example with regionId on Region and Id on the DTO
+            //CreateMap<Region, RegionDto>().ForMember(dest => dest.Id, options => options.MapFrom(src => src.RegionId) );
+
+            //to convert from DTO to the domain models
+            //CreateMap<Region, RegionDto>().ReverseMap();
+        }
+    }
+}
