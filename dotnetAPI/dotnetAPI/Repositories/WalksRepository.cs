8ff7e06d5242654fe3fe83595e7e5f523490cbef
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2022-11-17 14:42:28 -0800

    Walks repository added

---
+++ dotnetAPI/dotnetAPI/Repositories/WalksRepository.cs
@@ -0,0 +1,12 @@
+﻿using dotnetAPI.model;
+
+namespace dotnetAPI.Repositories {
+    public interface WalksRepository {
+        Task<IEnumerable<Walk>> GetAllWalks();
+        Task<Walk> GetWalkById(int id);
+        Task<Boolean> DelelteWalk(int id);
+        Task<Walk> AddWalk(Walk walk);
+        Task<Walk> UpdateWalk(Walk walk);
+
+    }
+}
---
+++ dotnetAPI/dotnetAPI/Repositories/WalksRepositoryImpl.cs
@@ -0,0 +1,32 @@
+﻿using dotnetAPI.Data;
+using dotnetAPI.model;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnetAPI.Repositories {
+    public class WalksRepositoryImpl : WalksRepository {
+        private readonly NZWalksDbContext nZWalksDbContext;
+
+        public WalksRepositoryImpl(NZWalksDbContext nZWalksDbContext) {
+            this.nZWalksDbContext = nZWalksDbContext;
+        }
+        public Task<Walk> AddWalk(Walk walk) {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DelelteWalk(int id) {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Walk>> GetAllWalks() {
+            return await nZWalksDbContext.Walks.ToListAsync();
+        }
+
+        public Task<Walk> GetWalkById(int id) {
+            throw new NotImplementedException();
+        }
+
+        public Task<Walk> UpdateWalk(Walk walk) {
+            throw new NotImplementedException();
+        }
+    }
+}
