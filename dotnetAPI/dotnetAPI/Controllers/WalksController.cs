07da9c01f44784a821e0dc8a3674b015b1fddba8
SamuelVZ <samuel.valencia.zamudio@hotmail.com>
2022-11-17 14:44:43 -0800

    Walks controller created

---
+++ dotnetAPI/dotnetAPI/Controllers/WalksController.cs
@@ -0,0 +1,22 @@
+﻿using dotnetAPI.model;
+using dotnetAPI.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnetAPI.Controllers {
+    [ApiController]
+    [Route("[controller]")]
+    public class WalksController : Controller {
+        private readonly WalkService walkService;
+
+        public WalksController(WalkService walkService) {
+            this.walkService = walkService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll() {
+            var walks = await walkService.GetAll();
+
+            return Ok(walks);
+        }
+    }
+}
